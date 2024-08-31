8f278e3477833ff7a12cb755c7605c5ed612339f
Sasikant Biswal <biswalsashikant@gmail.com>
2024-09-01 03:02:15 +0530

    getByRole queries added

--- react-testing/src/components/greet/Greet.test.tsx
+++ react-testing/src/components/greet/Greet.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Greet renders correctly", () => {
-  render(<Greet />);
-  const textElement = screen.getByText(/hello/i);
-  expect(textElement).toBeInTheDocument();
-});
+describe("Greet", () => {
+  test("renders correctly", () => {
+    render(<Greet />);
+    const textElement = screen.getByText(/hello/i);
+    expect(textElement).toBeInTheDocument();
+  });
 
-test("Greet renders with a name", () => {
-  render(<Greet name="Vishwas" />);
-  const textElement = screen.getByText("Hello Vishwas");
-  expect(textElement).toBeInTheDocument();
+  test("renders with a name", () => {
+    render(<Greet name="Vishwas" />);
+    const textElement = screen.getByText("Hello Vishwas");
+    expect(textElement).toBeInTheDocument();
+  });
 });
