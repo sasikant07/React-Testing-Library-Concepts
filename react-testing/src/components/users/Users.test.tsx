786f7de60520f7af23baa512f0becc0db5dc00f0
Sasikant Biswal <biswalsashikant@gmail.com>
2024-09-15 02:19:59 +0530

    Test for hanling ERROR using MSW

--- react-testing/src/components/users/Users.test.tsx
+++ react-testing/src/components/users/Users.test.tsx
@@ -1,11 +1,11 @@
 import { render, screen } from "@testing-library/react";
 import { server } from "../../mocks/server";
+import { rest } from "msw";
 import Users from "./Users";
 
 // beforeAll(() => server.listen());
 // afterEach(() => server.resetHandlers());
 // afterAll(() => server.close());
-
 
 describe("Users", () => {
   test("renders correctly", () => {
@@ -19,5 +19,21 @@ describe("Users", () => {
 
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+
+  // Hnadling Error using MSW
+  test("renders error", async () => {
+    server.use(
+      rest.get(
+        "https://jsonplaceholder.typicode.com/users",
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+
+    render(<Users />);
+    const error = await screen.findByText("Error fetching users");
+    expect(error).toBeInTheDocument();
   });
 });
